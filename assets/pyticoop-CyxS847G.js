9c126d738cd0ef61586128c57a422f302eb71360
AmauMaill <AmauMaill@users.noreply.github.com>
2024-12-15 00:29:50 +0000

    deploy: 3d12bd326de1524d359b7bd9a6b172119fd42136

---
+++ assets/pyticoop-CyxS847G.js
@@ -0,0 +1,5 @@
+const t=`# Pyticoop
+
+A Python library for the TiCoop supermarket (Brest).
+
+[GitHub](https://github.com/AmauMaill/pyticoop) [PyPI](https://pypi.org/project/pyticoop/)`;export{t as default};
